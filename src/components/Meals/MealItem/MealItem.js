f005cf54b513046c8b4c3e61cb4a5b4c7b1d3a30
MiteshDarda <mitesh.darda.0@gmai.com>
2022-09-04 16:27:12 +0530

    +MealItemForm +Input

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const price = `₹${props.price.toFixed(2)}`;
@@ -7,10 +8,12 @@ const MealItem = (props) => {
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
       </div>
-      <div className={classes.description}>{props.description}</div>
-      <div className={classes.price}>{price}</div>
-      <div></div>
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
     </li>
   );
 };
