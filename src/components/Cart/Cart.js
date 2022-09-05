9d2cc743f7053d14fc48119d733a4522f1b6265c
MiteshDarda <mitesh.darda.0@gmai.com>
2022-09-05 22:28:23 +0530

    link cart-modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -10,16 +11,20 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal onClose={props.onClose}>
+      {/* <div> */}
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span> <span>69</span>
       </div>
-      <div className={classes.action}>
-        <button className={classes["button--alt"]}>Close</button>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+      {/* </div> */}
+    </Modal>
   );
 };
 
